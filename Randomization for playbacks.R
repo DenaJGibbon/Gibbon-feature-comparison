599e3d7f54942925a8b19e4d5b7ad8b423d635e1
Dena Clink <denaclink@Denas-MacBook-Pro.local>
2023-07-22 22:16:53 -0400

    Update code.

--- Randomization for playbacks.R
+++ Randomization for playbacks.R
@@ -6,6 +6,7 @@ library(caret)
 library(dbscan)
 library(aricode)
 
+# Note for aplcluster used default q
 # MFCC Randomization ------------------------------------------------------
 
 # Read MFCCPlaybacks.csv
@@ -30,7 +31,7 @@ for (a in 1:length(UniqueRecorderMFCC)) {
   MFCCPlaybacksSingleRecorder <- subset(MFCCPlaybacks, Recorder == UniqueRecorderMFCC[a])
   
   # Perform randomization 20 times
-  for (b in 1:1) {
+  for (b in 1:20) {
     set.seed(b)
     
     # Split the data into training and test sets
@@ -52,7 +53,7 @@ for (a in 1:length(UniqueRecorderMFCC)) {
     TestPredictions <- predict(ml.model.rf.mfcc.original, newdata = TestData[, -c(26:28)])
     
     # Compute accuracy of the predictions
-    ConfMatrix <- confusionMatrix(TestData$Individual, TestPredictions)
+    ConfMatrix <- confusionMatrix(TestData$Individual, TestPredictions, mode='everything')
     AccuracyVal <- ConfMatrix$overall[1]
     
     # Store accuracy and recorder information
@@ -63,6 +64,13 @@ for (a in 1:length(UniqueRecorderMFCC)) {
                controlscale=TRUE,scale=3,n_neighbors=10)
     
     TempCluster <- hdbscan(AcousticSignals.umap$layout[,1:2], minPts = 20)
+    # TempCluster <- apcluster::apcluster(
+    #   apcluster::negDistMat(r = 2),# q=0,
+    #   AcousticSignals.umap$layout[,1:2],
+    #   maxits = 5000,
+    #   convits = 500,
+    #   nonoise = T
+    # )
     N.cluster <- length(unique(TempCluster$cluster))
     N.Individual <- length(unique(TrainingData$Individual))
     NMI.val <- NMI(TempCluster$cluster,TrainingData$Individual)
@@ -72,17 +80,18 @@ for (a in 1:length(UniqueRecorderMFCC)) {
     
     # Print and write RandomizationAccuracyMFCC to a CSV file
     print(TempMFCCRow)
-    write.csv(RandomizationAccuracyMFCC, 'data/randomization/RandomizationAccuracyMFCC.csv')
+    write.csv(RandomizationAccuracyMFCC, 'data/randomization_hdbscan/RandomizationAccuracyMFCC.csv')
   }
 }
 
 # BirdNET Randomization ------------------------------------------------------
-
 # Read BirdNETPlaybacks.csv
-BirdNETPlaybacks <- read.csv('data/BirdNetFeaturesplayback.csv')
+file_names <- dir('data/BirdNET/', full.names = T)#where you have your files
+
+BirdNETPlaybacks <- do.call(rbind,lapply(file_names,read.csv))
 
 # Extract the recorder information from the 'Recording' column
-BirdNETPlaybacks$Recorder <- as.factor(str_split_fixed(BirdNETPlaybacks$Recording, pattern = '_', n = 2)[, 1])
+BirdNETPlaybacks$Recorder <- as.factor(BirdNETPlaybacks$Recorder)
 
 # Get unique recorders
 UniqueRecorderBirdNET <- unique(BirdNETPlaybacks$Recorder)
@@ -97,10 +106,10 @@ RandomizationAccuracyBirdNET <- data.frame()
 for (a in 1:length(UniqueRecorderBirdNET)) {
   
   # Subset the data for the current recorder
-  BirdNETPlaybacksSingleRecorder <- subset(BirdNETPlaybacks, Recorder == UniqueRecorderBirdNET[a])
+  BirdNETPlaybacksSingleRecorder <- droplevels(subset(BirdNETPlaybacks, Recorder == UniqueRecorderBirdNET[a]))
   
   # Perform randomization 20 times
-  for (b in 1:1) {
+  for (b in 1:20) {
     set.seed(b)
     
     # Split the data into training and test sets
@@ -111,7 +120,7 @@ for (a in 1:length(UniqueRecorderBirdNET)) {
     
     # Build a random forest model using the training data
     ml.model.rf.BirdNET.original <- randomForest::randomForest(
-      x = TrainingData[, -c(2049,2050,2051,2053)],
+      x = TrainingData[, -c(2048:2050,2052)],
       y = TrainingData$Individual,
       ntree = 500,
       random_state = 0,
@@ -119,7 +128,7 @@ for (a in 1:length(UniqueRecorderBirdNET)) {
     )
     
     # Make predictions on the test data
-    TestPredictions <- predict(ml.model.rf.BirdNET.original, newdata = TestData[, -c(2049,2050,2051,2053)])
+    TestPredictions <- predict(ml.model.rf.BirdNET.original, newdata = TestData[, -c(2048:2050,2052)])
     
     # Compute accuracy of the predictions
     ConfMatrix <- confusionMatrix(TestData$Individual, TestPredictions)
@@ -129,7 +138,7 @@ for (a in 1:length(UniqueRecorderBirdNET)) {
     Recorder <- UniqueRecorderBirdNET[a]
     Features <- 'BirdNet'
     
-    AcousticSignals.umap <- umap::umap( TrainingData[, -c(2049,2050,2051,2053)],
+    AcousticSignals.umap <- umap::umap( TrainingData[, -c(2048:2050,2052)],
                                         controlscale=TRUE,scale=3,n_neighbors=10)
     
     TempCluster <- hdbscan(AcousticSignals.umap$layout[,1:2], minPts = 20)
@@ -142,14 +151,15 @@ for (a in 1:length(UniqueRecorderBirdNET)) {
     
     # Print and write RandomizationAccuracyBirdNET to a CSV file
     print(TempBirdNETRow)
-    write.csv(RandomizationAccuracyBirdNET, 'data/randomization/RandomizationAccuracyBirdNET.csv')
+    write.csv(RandomizationAccuracyBirdNET, 'data/randomization_hdbscan/RandomizationAccuracyBirdNET.csv')
   }
 }
 
 # VGGish Randomization ------------------------------------------------------
-
 # Read VGGishPlaybacks.csv
-VGGishPlaybacks <- read.csv('data/VGGishDFPlayback.csv')
+file_names <- dir('data/VGGish/', full.names = T)#where you have your files
+
+VGGishPlaybacks <- do.call(rbind,lapply(file_names,read.csv))
 
 # Extract the recorder information from the 'Recording' column
 VGGishPlaybacks$Recorder <- str_split_fixed(VGGishPlaybacks$RecorderID, pattern = '_', n = 2)[, 1]
@@ -170,7 +180,7 @@ for (a in 1:length(UniqueRecorderVGGish)) {
   VGGishPlaybacksSingleRecorder <- subset(VGGishPlaybacks, Recorder == UniqueRecorderVGGish[a])
   
   # Perform randomization 20 times
-  for (b in 1:1) {
+  for (b in 1:20) {
     set.seed(b)
     
     # Split the data into training and test sets
@@ -212,14 +222,14 @@ for (a in 1:length(UniqueRecorderVGGish)) {
     
     # Print and write RandomizationAccuracyVGGish to a CSV file
     print(TempVGGishRow)
-    write.csv(RandomizationAccuracyVGGish, 'data/randomization/RandomizationAccuracyVGGish.csv')
+    write.csv(RandomizationAccuracyVGGish, 'data/randomization_hdbscan/RandomizationAccuracyVGGish.csv')
   }
 }
 
 # wav2vec2 Randomization ------------------------------------------------------
 
 # Read wav2vec2Playbacks.csv
-wav2vec2Playbacks <- read.csv('data/wav2vecDFPlayback.csv')
+wav2vec2Playbacks <- read.csv('data/wav2vecmeansdDFPlayback.csv')
 
 # Extract the recorder information from the 'Recording' column
 wav2vec2Playbacks$Recorder <- str_split_fixed(wav2vec2Playbacks$RecorderID, pattern = '_', n = 2)[, 1]
@@ -240,7 +250,7 @@ for (a in 1:length(UniqueRecorderwav2vec2)) {
   wav2vec2PlaybacksSingleRecorder <- subset(wav2vec2Playbacks, Recorder == UniqueRecorderwav2vec2[a])
   
   # Perform randomization 20 times
-  for (b in 1:1) {
+  for (b in 1:20) {
     set.seed(b)
     
     # Split the data into training and test sets
@@ -251,7 +261,7 @@ for (a in 1:length(UniqueRecorderwav2vec2)) {
     
     # Build a random forest model using the training data
     ml.model.rf.wav2vec2.original <- randomForest::randomForest(
-      x = TrainingData[, -c(769,771:773)],
+      x = TrainingData[, -c(41,42,44)],
       y = TrainingData$Individual,
       ntree = 500,
       random_state = 0,
@@ -259,7 +269,7 @@ for (a in 1:length(UniqueRecorderwav2vec2)) {
     )
     
     # Make predictions on the test data
-    TestPredictions <- predict(ml.model.rf.wav2vec2.original, newdata = TestData[, -c(769,771:773)])
+    TestPredictions <- predict(ml.model.rf.wav2vec2.original, newdata = TestData[, -c(41,42,44)])
     
     # Compute accuracy of the predictions
     ConfMatrix <- confusionMatrix(TestData$Individual, TestPredictions)
@@ -269,21 +279,27 @@ for (a in 1:length(UniqueRecorderwav2vec2)) {
     Recorder <- UniqueRecorderwav2vec2[a]
     Features <- 'wav2vec2'
     
-    AcousticSignals.umap <- umap::umap( TrainingData[, -c(769,771:773)],
+    AcousticSignals.umap <- umap::umap( TrainingData[, -c(41,42,44)],
                                         controlscale=TRUE,scale=3,n_neighbors=10)
     
-    TempCluster <- hdbscan(AcousticSignals.umap$layout[,1:2], minPts = 20)
-    
-    N.cluster <- length(unique(TempCluster$cluster))
+    #TempCluster <- hdbscan(AcousticSignals.umap$layout[,1:2], minPts = 20)
+    TempCluster <- apcluster::apcluster(
+      apcluster::negDistMat(r = 2),# q=0,
+      AcousticSignals.umap$layout[,1:2],
+      maxits = 5000,
+      convits = 500,
+      nonoise = T
+    )
+    N.cluster <- length(unique(TempCluster@idx))
     N.Individual <- length(unique(TrainingData$Individual))
-    NMI.val <- NMI(TempCluster$cluster,TrainingData$Individual)
+    NMI.val <- NMI(TempCluster@idx,TrainingData$Individual)
     Tempwav2vec2Row <- cbind.data.frame(AccuracyVal, Recorder,Features,N.cluster,N.Individual,NMI.val)
     
     RandomizationAccuracywav2vec2 <- rbind.data.frame(RandomizationAccuracywav2vec2, Tempwav2vec2Row)
     
     # Print and write RandomizationAccuracywav2vec2 to a CSV file
     print(Tempwav2vec2Row)
-    write.csv(RandomizationAccuracywav2vec2, 'data/randomization/RandomizationAccuracywav2vec2.csv')
+    write.csv(RandomizationAccuracywav2vec2, 'data/randomization_affinity/RandomizationAccuracywav2vec2meansd.csv')
   }
 }
 
@@ -311,7 +327,7 @@ for (a in 1:length(UniqueRecorderAcousticIndices)) {
   AcousticIndicesPlaybacksSingleRecorder <- subset(AcousticIndicesPlaybacks, Recorder == UniqueRecorderAcousticIndices[a])
   
   # Perform randomization 20 times
-  for (b in 1:1) {
+  for (b in 1:20) {
     set.seed(b)
     
     # Split the data into training and test sets
@@ -343,7 +359,6 @@ for (a in 1:length(UniqueRecorderAcousticIndices)) {
                                         controlscale=TRUE,scale=3,n_neighbors=10)
     
     TempCluster <- hdbscan(AcousticSignals.umap$layout[,1:2], minPts = 20)
-    
     N.cluster <- length(unique(TempCluster$cluster))
     N.Individual <- length(unique(TrainingData$Individual))
     NMI.val <- NMI(TempCluster$cluster,TrainingData$Individual)
@@ -353,10 +368,122 @@ for (a in 1:length(UniqueRecorderAcousticIndices)) {
     
     # Print and write RandomizationAccuracyAcousticIndices to a CSV file
     print(TempAcousticIndicesRow)
-    write.csv(RandomizationAccuracyAcousticIndices, 'data/randomization/RandomizationAccuracyAcousticIndices.csv')
+    write.csv(RandomizationAccuracyAcousticIndices, 'data/randomization_hdbscan/RandomizationAccuracyAcousticIndices.csv')
   }
 }
 
 
+# Plots for randomization -------------------------------------------------
 
+# Load required libraries
+library(ggplot2)
+library(cowplot)
+library(viridis)
 
+# Load and process data from the 'data/randomization_hdbscan/' directory
+file_names <- dir('data/randomization_hdbscan/', full.names = TRUE) # List all file names in the directory
+
+# Combine data from all files into a single data frame using read.csv and do.call(rbind, ...)
+AllPlaybacksRand <- do.call(rbind, lapply(file_names, read.csv))
+
+# Convert 'AccuracyVal' column to percentage scale
+AllPlaybacksRand$AccuracyVal <- AllPlaybacksRand$AccuracyVal * 100
+
+# Convert factors for 'Recorder' and 'Features' columns to meaningful labels
+AllPlaybacksRand$Recorder <- as.factor(AllPlaybacksRand$Recorder)
+levels(AllPlaybacksRand$Recorder) <- c('M1 (0 m)', 'M2 (50 m)', 'M3 (100 m)', 'M4 (150 m)',
+                                       'M5 (200 m)', 'M6 (250 m)', 'M7 (300 m)')
+
+AllPlaybacksRand$Features <- as.factor(AllPlaybacksRand$Features)
+levels(AllPlaybacksRand$Features) <- c('Acoustic Indices', 'BirdNET', 'MFCCs',
+                                       'VGGish', 'Wav2Vec2')
+
+# Create ggplot2 plot 'PlotA' for Classification Accuracy by Features and Recorder
+PlotA <- ggboxplot(data = AllPlaybacksRand,
+                   color = 'Recorder', fill = 'Recorder', y = 'AccuracyVal', x = 'Features') +
+  scale_color_manual(values = viridis::viridis(7)) +
+  scale_fill_manual(values = viridis::viridis(7)) +
+  ylab('Classification Accuracy')
+
+# Create ggplot2 plot 'PlotBHDBSCAN' for Normalized Mutual Information (HDBSCAN) by Features and Recorder
+PlotBHDBSCAN <- ggboxplot(data = AllPlaybacksRand,
+                          color = 'Recorder', fill = 'Recorder', y = 'NMI.val', x = 'Features') +
+  scale_color_manual(values = viridis::viridis(7)) +
+  scale_fill_manual(values = viridis::viridis(7)) +
+  ylab('Normalized Mutual Information (HDBSCAN)')
+
+# Calculate the deviation between 'N.Individual' and 'N.cluster' columns and add it as 'Deviation' column
+AllPlaybacksRand$Deviation <- AllPlaybacksRand$N.Individual - AllPlaybacksRand$N.cluster
+
+# Create ggplot2 plot 'PlotCHDBSCAN' for Number of Clusters (HDBSCAN) by Features and Recorder
+PlotCHDBSCAN <- ggboxplot(data = AllPlaybacksRand,
+                          color = 'Recorder', fill = 'Recorder', y = 'N.cluster', x = 'Features', outlier.shape = NA) +
+  scale_color_manual(values = viridis::viridis(7)) +
+  scale_fill_manual(values = viridis::viridis(7)) +
+  ylab('Number of clusters (HDBSCAN)') +
+  geom_hline(yintercept = 12, linetype = 'dashed') + ylim(0, 25) + xlab('')
+
+# Create a cowplot grid with PlotA and PlotB, labeled as 'A' and 'B', respectively, in 2 rows
+cowplot::plot_grid(PlotA, PlotBHDBSCAN, labels = c('A', 'B'), nrow = 2, label_x = 0.95)
+
+# Repeat the process for the 'data/randomization_affinity/' directory
+file_names <- dir('data/randomization_affinity/', full.names = TRUE)
+AllPlaybacksRand <- do.call(rbind, lapply(file_names, read.csv))
+AllPlaybacksRand$AccuracyVal <- AllPlaybacksRand$AccuracyVal * 100
+AllPlaybacksRand$Recorder <- as.factor(AllPlaybacksRand$Recorder)
+levels(AllPlaybacksRand$Recorder) <- c('M1 (0 m)', 'M2 (50 m)', 'M3 (100 m)', 'M4 (150 m)',
+                                       'M5 (200 m)', 'M6 (250 m)', 'M7 (300 m)')
+AllPlaybacksRand$Features <- as.factor(AllPlaybacksRand$Features)
+levels(AllPlaybacksRand$Features) <- c('Acoustic Indices', 'BirdNET', 'MFCCs',
+                                       'VGGish', 'Wav2Vec2')
+
+# Create ggplot2 plot 'PlotBaffinity' for Normalized Mutual Information (affinity) by Features and Recorder
+PlotBaffinity <- ggboxplot(data = AllPlaybacksRand,
+                           color = 'Recorder', fill = 'Recorder', y = 'NMI.val', x = 'Features') +
+  scale_color_manual(values = viridis::viridis(7)) +
+  scale_fill_manual(values = viridis::viridis(7)) +
+  ylab('Normalized Mutual Information (affinity)') + xlab('')
+
+# Calculate the deviation between 'N.Individual' and 'N.cluster' columns and add it as 'Deviation' column
+AllPlaybacksRand$Deviation <- AllPlaybacksRand$N.Individual - AllPlaybacksRand$N.cluster
+
+# Create ggplot2 plot 'PlotCaffinity' for Number of Clusters (affinity) by Features and Recorder
+PlotCaffinity <- ggboxplot(data = AllPlaybacksRand,
+                           color = 'Recorder', fill = 'Recorder', y = 'N.cluster', x = 'Features', outlier.shape = NA) +
+  scale_color_manual(values = viridis::viridis(7)) +
+  scale_fill_manual(values = viridis::viridis(7)) +
+  ylab('Number of clusters (affinity)') +
+  geom_hline(yintercept = 12, linetype = 'dashed') + ylim(0, 25) + xlab('')
+
+# Create a cowplot grid with PlotBaffinity and PlotBHDBSCAN, labeled as 'A' and 'B', respectively, in 2 rows
+cowplot::plot_grid(PlotBaffinity, PlotBHDBSCAN, labels = c('A', 'B'), nrow = 2, label_x = 0.95)
+
+# Create a cowplot grid with PlotCaffinity and PlotCHDBSCAN, labeled as 'A' and 'B', respectively, in 2 rows
+cowplot::plot_grid(PlotCaffinity, PlotCHDBSCAN, labels = c('A', 'B'), nrow = 2, label_x = 0.95)
+
+# Number of unique playbacks (No plot or table generated for this section)
+uniquepd <- str_split_fixed(MFCCPlaybacks$Recording, pattern = '_', n = 2)[, 2]
+uniquepd <- substr((uniquepd), 1, 13)
+unique(uniquepd)
+
+# Calculate the mean and standard deviation by feature type and recorder distance
+summary_data <- AllPlaybacksRand %>%
+  group_by(Features, Recorder) %>%
+  summarise(mean_value = mean(AccuracyVal, na.rm = TRUE),
+            sd_value = sd(AccuracyVal, na.rm = TRUE), .groups = "drop") %>%
+  arrange(desc(mean_value)) # Order the table by recorder_distance
+
+# Create the kable table with custom font
+my_table <- kable(summary_data) %>%
+  kable_styling()
+
+# Save the kable table as a PDF file named 'summary_data.pdf'
+#kableExtra::save_kable(my_table, file = 'summary_data.pdf')
+
+# Add table formatting (optional, customize as needed)
+my_table <- kable_styling(my_table, full_width = FALSE, latex_options = "hold_position", font_size = )
+
+# Display the summary_data table with HTML formatting and font size of 7
+summary_data %>%
+  kable("html") %>%
+  kable_styling(font_size = 7)
